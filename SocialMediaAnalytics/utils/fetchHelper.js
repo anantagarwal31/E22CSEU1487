9252e04f3636bc3d8930c1f6f551c1d4a6d8f784
aa311003 <anantagarwal2003@gmail.com>
2025-04-04 11:19:08 +0530

    fetchHelper updated

---
+++ SocialMediaAnalytics/utils/fetchHelper.js
@@ -0,0 +1,7 @@
+export const fetchJSON = async (url) => {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+};
